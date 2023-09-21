602977a71e31aad0a86066716de4b5e960f10121
vadim-cvy <vadim.cvy@gmail.com>
2023-09-21 23:00:51 +0300

    Add vendor autoload

--- init.php
+++ init.php
@@ -6,4 +6,6 @@ define( 'JLX_ASSETS_PATH', JLX_ROOT_PATH . 'assets/' );
 define( 'JLX_JS_PATH', JLX_ASSETS_PATH . 'js/dist/' );
 define( 'JLX_CSS_PATH', JLX_ASSETS_PATH . 'css/' );
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 \MyApp\Example::do_something();
