ef98f2a5938a37499a5f33c60250f571f6dd7ec3
vadim-cvy <vadim.cvy@gmail.com>
2023-10-21 14:18:12 +0300

    Add template-parts dir path constant

--- init.php
+++ init.php
@@ -2,6 +2,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 define( 'JLX_ROOT_PATH', __DIR__ . '/' );
+define( 'JLX_TEMPLATES_PATH', JLX_ROOT_PATH . 'template-parts/' );
 define( 'JLX_ASSETS_PATH', JLX_ROOT_PATH . 'assets/' );
 define( 'JLX_JS_PATH', JLX_ASSETS_PATH . 'js/dist/' );
 define( 'JLX_CSS_PATH', JLX_ASSETS_PATH . 'css/' );
