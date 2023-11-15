168ce0f9a837ca9f536c11d0cae5aa903e9282f1
Vadim Cherepenichev <vadim.cvy@gmail.com>
2023-11-15 22:50:07 +0200

    Update constants names prefixes

--- init.php
+++ init.php
@@ -1,11 +1,11 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'JLX_ROOT_PATH', __DIR__ . '/' );
-define( 'JLX_TEMPLATES_PATH', JLX_ROOT_PATH . 'template-parts/' );
-define( 'JLX_ASSETS_PATH', JLX_ROOT_PATH . 'assets/' );
-define( 'JLX_JS_PATH', JLX_ASSETS_PATH . 'js/dist/' );
-define( 'JLX_CSS_PATH', JLX_ASSETS_PATH . 'css/' );
+define( 'MYAPP_ROOT_PATH', __DIR__ . '/' );
+define( 'MYAPP_TEMPLATES_PATH', MYAPP_ROOT_PATH . 'template-parts/' );
+define( 'MYAPP_ASSETS_PATH', MYAPP_ROOT_PATH . 'assets/' );
+define( 'MYAPP_JS_PATH', MYAPP_ASSETS_PATH . 'js/dist/' );
+define( 'MYAPP_CSS_PATH', MYAPP_ASSETS_PATH . 'css/' );
 
 require_once __DIR__ . '/vendor/autoload.php';
 
