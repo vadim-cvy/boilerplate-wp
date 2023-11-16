c9a6393fe14963704cbc4528e486061a9209beb6
Vadim Cherepenichev <vadim.cvy@gmail.com>
2023-11-16 18:23:19 +0200

    Fix assets util "app namespace is not set" error

--- theme.functions.php
+++ theme.functions.php
@@ -4,5 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 require_once __DIR__ . '/init.php';
 
 // todo: exeucute `composer require cvy/util-wp-assets`
+Cvy\WP\Assets\Main::set_app_namespace( 'myapp' );
+
 // todo: if Astra child theme - add 'astra' dependency
 (new Cvy\WP\Assets\CSS( '../../../style.css' ))->enqueue();
