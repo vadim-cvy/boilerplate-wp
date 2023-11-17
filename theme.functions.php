e1ebbd51fa7272797f201fd143654b0ba41b6bf0
Vadim Cherepenichev <vadim.cvy@gmail.com>
2023-11-17 04:49:40 +0200

    Correct misspell

--- theme.functions.php
+++ theme.functions.php
@@ -6,5 +6,5 @@ require_once __DIR__ . '/init.php';
 // todo: exeucute `composer require cvy/util-wp-assets`
 Cvy\WP\Assets\Main::set_app_namespace( 'myapp' );
 
-// todo: if Astra child theme - add 'astra' dependency
+// todo: if Astra child theme - add 'astra-theme-css' dependency
 (new Cvy\WP\Assets\CSS( '../../../style.css' ))->enqueue();
