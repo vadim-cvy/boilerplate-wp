c204459253354f2809816fc58e6521060fd9b89a
Vadim Cherepenichev <vadim.cvy@gmail.com>
2023-11-23 16:06:01 +0200

    Prevent style.css enqueuing in dashboard

--- theme.functions.php
+++ theme.functions.php
@@ -6,5 +6,8 @@ require_once __DIR__ . '/init.php';
 // todo: exeucute `composer require cvy/util-wp-assets`
 Cvy\WP\Assets\Main::set_app_namespace( 'myapp' );
 
-// todo: if Astra child theme - add 'astra-theme-css' dependency
-(new Cvy\WP\Assets\CSS( '../../../style.css' ))->enqueue();
+if ( ! is_admin() )
+{
+  // todo: if Astra child theme - add 'astra-theme-css' dependency
+  (new Cvy\WP\Assets\CSS( '../../../style.css' ))->enqueue();
+}
